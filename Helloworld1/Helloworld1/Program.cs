a72934e5322c71355335b1209c748438660c86c5
pallaviroyal <pallaviroyal007@gmail.com>
2025-07-21 20:30:46 +0530

    demo

---
+++ Helloworld1/Helloworld1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+        int a = 12;
+        string name = "Pallavi";
+        double basic = 2233.88;
+        Console.WriteLine("A value is " + a);
+         Console.WriteLine("name is " + name);
+         Console.WriteLine("salary id " +basic);
+
+        
+        }
+    }
+}
