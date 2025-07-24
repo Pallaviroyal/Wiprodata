f44f8fcb8a10753ffdf80ba74641abb69e4fd86a
Pallavipoola <116418245+Pallaviroyal@users.noreply.github.com>
2025-07-24 13:37:21 +0530

    Add files via upload

---
+++ Bitwise4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    internal class Bitwise4
+    {
+        public static void main()
+        {
+            int a = 7;
+            Console.WriteLine();
+        }
+    }
+}
